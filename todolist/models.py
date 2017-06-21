8e177eb32eea986748f316c7bf5b77630aa5bd64
Dmytro Gierman <mryakzp@gmail.com>
2017-06-21 18:01:55 +0300

    Squashed commit: -create django project and app -created minimal index page. enabled todolist app in settings.py -created modelas for 'project' and 'task' entities -add authentication
    Project was moved to new folder, and reinitiated, coz I forget to change git global name|email
    when started working on the project.

---
+++ todolist/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    date_created = models.DateTimeField()
+    colour = models.CharField(max_length=20)
+
+
+class Task(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    priority = models.CharField(max_length=20)
+    date_to_finish = models.DateTimeField()
+    finished = models.BooleanField(default=False)
